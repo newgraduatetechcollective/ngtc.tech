346814488ab9a4ac202928331cf1294b4477e781
Saniya Shaikh <sanushk95@gmail.com>
2020-09-01 16:36:36 -0400

    Created front-end pages, and defined new routes.

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'New Graduate Tech Collective: Home' });
+    res.render('index', { title: 'Home | New Graduate Tech Collective: Home' });
 });
 
 router.get('/about', function(req, res, next) {
-    res.render('about', { title: 'ngtc.tech' });
+    res.render('about', { title: 'About | ngtc.tech' });
 })
 
 router.get('/works', function(req, res, next) {
-    res.render('works', { title: 'ngtc.tech' });
+    res.render('works', { title: 'Our Works | ngtc.tech' });
 })
 
 router.get('/team', function(req, res, next) {
-    res.render('team', { title: 'ngtc.tech' });
+    res.render('team', { title: 'Team | ngtc.tech' });
 })
 
 router.get('/contact', function(req, res, next) {
-    res.render('contact', { title: 'ngtc.tech' });
+    res.render('contact', { title: 'Contact Us | ngtc.tech' });
 })
 
 module.exports = router;
